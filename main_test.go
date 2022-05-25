008c01cf7844b47ec8e72fda3ded00d1674cf327
matthias.lauss <matthias.lauss@icloud.com>
2022-05-25 16:39:37 +0200

    github actions file

--- main_test.go
+++ main_test.go
@@ -17,9 +17,13 @@ import (
 
  func TestMain(m *testing.M) {
 	 a.Initialize(
-		 os.Getenv("APP_DB_USERNAME"),
-		 os.Getenv("APP_DB_PASSWORD"),
-		 os.Getenv("APP_DB_NAME"))
+		//  os.Getenv("APP_DB_USERNAME"),
+		//  os.Getenv("APP_DB_PASSWORD"),
+		//  os.Getenv("APP_DB_NAME"))
+		"postgres",
+		"asdf",
+		"postgres"
+	 )
  
 	 ensureTableExists()
 	 code := m.Run()
